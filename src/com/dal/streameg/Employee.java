9c05c84cd94dc5107817328651587fcb56cc6056
vs353 <130337694+vs353@users.noreply.github.com>
2023-04-17 11:23:51 +0530

    Vaishnavi - Java_Batch01
    Task - Predicate Implementations

---
+++ src/com/dal/streameg/Employee.java
@@ -0,0 +1,58 @@
+package com.dal.streameg;
+
+public class Employee {
+	private String fname;
+	private String lname;
+	private String dept;
+	private int salary;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String fname, String lname, String dept, int salary) {
+		super();
+		this.fname = fname;
+		this.lname = lname;
+		this.dept = dept;
+		this.salary = salary;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [fname=" + fname + ", lname=" + lname + ", dept=" + dept + ", salary=" + salary + "]";
+	}
+
+}
